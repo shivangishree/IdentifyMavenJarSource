6bb57ce0c0e1f1fdf49ba37a80b9697c3346b625
Shivangi Shree <shivangi.shree@boeing.com>
2020-01-10 12:16:16 +0530

    With comments

--- ReadFile/src/main/java/read/ReadLine.java
+++ ReadFile/src/main/java/read/ReadLine.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 public class ReadLine {
     public static final String ANSI_RED_TEXT = "\033[0;31m";
     public static final String BLACK_BOLD = "\033[1;30m";
+    public static final String BLACK = "\033[0;30m";
     List<String> listRemoteRepositories = new ArrayList<String>();
     Map<String, List<String>> mySresNexusMap = new HashMap<String, List<String>>();
     Map<String, List<String>> myJarSourceMap = new HashMap<String, List<String>>();
@@ -20,7 +21,7 @@ public class ReadLine {
 
     public static void main(String[] args) {
         ReadLine readLine = new ReadLine();
-        File file = new File("C:\\Users\\zi715e\\.m2");
+        File file = new File("C:\\Users\\zi715e\\Test");
         readLine.listAllFiles(file);
         readLine.getListSresNexus(readLine.listRemoteRepositories);
         readLine.printRepositorySourceList();
@@ -30,7 +31,10 @@ public class ReadLine {
         readLine.printgetListJarSource();
     }
 
-    // Uses listFiles method
+    // It checks whether it is directory or file.
+    // If it's directory it again calls itself(recursion),till it finds file.
+    //If it's a file it calls checkRemoteRepository to check whether it is remote repository or not.
+    // If it's _remote.repository then it reads it's content.
     public void listAllFiles(File folder) {
         File[] fileNames = folder.listFiles();
         for (File file : fileNames) {
@@ -44,10 +48,13 @@ public class ReadLine {
         }
     }
 
+    //Checks Whether file is ._remote.repositories or not
     private boolean checkRemoteRepository(String fileName) {
         return fileName.equals("_remote.repositories");
     }
 
+    //It reads the file line by line.
+    //It calls extractPomWar(line); for each line
     private void readContent(File file) {
         Scanner scanner = null;
         try {
@@ -65,6 +72,7 @@ public class ReadLine {
         }
     }
 
+    //It add list of jar/pom lines in listRemoteRepositories List.
     private void extractPomWar(String line) {
         if (line.contains(">")) {
             listRemoteRepositories.add(line);
@@ -72,6 +80,7 @@ public class ReadLine {
 
     }
 
+    //Creates a map of Source to jars
     private void getListSresNexus(List<String> listRemoteRepositories) {
         for (String line : listRemoteRepositories) {
             extractsource(line);
@@ -82,6 +91,8 @@ public class ReadLine {
         }
     }
 
+
+    //creates a map jars to source
     private void getListJarSource(List<String> listRemoteRepositories){
         for (String line : listRemoteRepositories) {
             extractsource(line);
@@ -98,6 +109,13 @@ public class ReadLine {
         jar = line.substring(0, startIndex);
     }
 
+    private void printRepositorySourceList(){
+        System.out.println(BLACK_BOLD + "List of Jar Source");
+        for (Map.Entry mapElement : mySresNexusMap.entrySet()) {
+            String key = (String) mapElement.getKey();
+            System.out.println(BLACK + key);
+        }
+    }
     private void printRepositoryList() {
         for (Map.Entry mapElement : mySresNexusMap.entrySet()) {
             String key = (String) mapElement.getKey();
@@ -105,17 +123,9 @@ public class ReadLine {
             List<String> jars = (List<String>) mapElement.getValue();
             int counter = 1;
             for (String jar : jars) {
-                System.out.println(counter++ +". " + jar);
+                System.out.println(BLACK + counter++ +". " + jar);
             }
             System.out.println("_________________________________________________________________");
-        }
-    }
-
-    private void printRepositorySourceList(){
-        System.out.println(BLACK_BOLD + "List of Jar Source");
-        for (Map.Entry mapElement : mySresNexusMap.entrySet()) {
-            String key = (String) mapElement.getKey();
-            System.out.println(key);
         }
     }
 
@@ -124,13 +134,13 @@ public class ReadLine {
         int counter = 1;
         for (Map.Entry mapElement : myJarSourceMap.entrySet()) {
             String key = (String) mapElement.getKey();
-            System.out.println(counter++ +". " +key);
+            System.out.println(BLACK + counter++ +". " +key);
             List<String> srcs = (List<String>) mapElement.getValue();
             for (String src : srcs) {
                 if(srcs.size()>1)
                     System.out.println(ANSI_RED_TEXT + src);
                 else
-                    System.out.println(src);
+                    System.out.println(BLACK + src);
             }
             System.out.println("_________________________________________________________________");
         }
